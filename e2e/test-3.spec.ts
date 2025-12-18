c30d23f00ae64bb758aa613cf72a3bbe2a9663e3
imnotkatya <151066476+imnotkatya@users.noreply.github.com>
2025-12-18 11:05:54 +0300

    third test

---
+++ e2e/test-3.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+ 
+  await page.goto('http://localhost:5173/');
+  await page.getByText('Нажмите в любом месте или перетащите Excel файл Поддерживаются файлы .xlsx, .xls').click();
+
+
+
+  const fileInput = page.locator('input[type="file"]');
+  await fileInput.setInputFiles('dist/waterfall.xlsx');
+await expect(page.getByRole('img'))
+  .toMatchAriaSnapshot(`- img: /Patients\\s*\\(N=\\s*\\d+\\)\\s*Change,%\\s*−\\d+\\s*−\\d+\\s*−\\d+\\s*−\\d+\\s*−\\d+\\s*0\\s*\\d+\\s*\\d+\\s*\\d+\\s*\\d+\\s*\\d+\\s*P21\\s*P7\\s*P5\\s*P25\\s*P15\\s*P17\\s*P3\\s*P22\\s*P11\\s*P12\\s*P13\\s*P14\\s*P19\\s*P20\\s*P10\\s*P9\\s*P8\\s*P6\\s*P18\\s*P4\\s*P2\\s*P23\\s*P16\\s*P24\\s*P1\\s*first_category\\s*second_category\\s*third_category\\s*fourth_category/`)
+  await expect(page.getByRole('img')).toBeVisible();
+});
