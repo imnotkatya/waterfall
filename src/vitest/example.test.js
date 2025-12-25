e288773a0da772a4f0796b9a1d2ad71dd24b4210
imnotkatya <151066476+imnotkatya@users.noreply.github.com>
2025-12-25 13:01:42 +0300

    import file check

--- src/vitest/example.test.js
+++ src/vitest/example.test.js
@@ -1,47 +1,30 @@
-import { describe, test, expect, beforeAll, afterAll } from "vitest";
-import { chromium } from "playwright";
+import * as XLSX from "xlsx";
+import * as fs from "fs";
+import * as path from "path";
+import { it, expect } from "vitest";
 
-describe("Waterfall Plot integration tests", () => {
-  let browser;
-  let page;
+it("import correct file", () => {
+  const filePath = path.resolve("dist/waterfall.xlsx");
+  const fileBuffer = fs.readFileSync(filePath);
+  const workbook = XLSX.read(fileBuffer, { type: "buffer" });
 
-  beforeAll(async () => {
-    browser = await chromium.launch();
-    page = await browser.newPage();
+  const combinedJson = {};
+  const sheetNames = [
+    workbook.SheetNames[1],
+    workbook.SheetNames[2],
+    workbook.SheetNames[3],
+  ];
+
+  sheetNames.forEach((sheetName, index) => {
+    const key = index + 1;
+    combinedJson[key] = {
+      name: sheetName,
+      data: XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]),
+    };
   });
 
-  afterAll(async () => {
-    await browser.close();
-  });
-
-  test("Загрузка Excel файла", async () => {
-    await page.goto("http://localhost:5173/");
-
-    const fileInput = await page.locator('input[type="file"]');
-    await fileInput.setInputFiles("dist/waterfall.xlsx");
-
-    await page.waitForTimeout(2000);
-
-    const svg = await page.locator("svg");
-    expect(svg).not.toBeNull();
-
-    const img = await page.locator("img");
-    expect(img).not.toBeNull();
-
-    const heading = await page.locator("h1");
-    const headingText = await heading?.textContent();
-    expect(headingText).toBe("Waterfall Plot");
-
-    const bodyText = await page.textContent("body");
-    expect(bodyText).toContain("first_category");
-    expect(bodyText).toContain("second_category");
-    expect(bodyText).toContain("third_category");
-    expect(bodyText).toContain("fourth_category");
-
-    expect(bodyText).toContain("Change,%");
-    expect(bodyText).toMatch("Patients (N= 92)");
-
-    expect(bodyText).toContain("P21");
-    expect(bodyText).toContain("P1");
-  });
+  expect(sheetNames.length).toBe(3);
+  expect(sheetNames[0]).toBe("data");
+  expect(sheetNames[1]).toBe("settings");
+  expect(sheetNames[2]).toBe("styles");
 });
